d9fec37a2d2c4e3c8503a08d9b00ebde10d6edfb
dara <dara@37be14ff-8d20-0410-9e9a-d2f6958099bb>
2006-11-27 07:58:58 +0000

    fixed FakeCGI to pass the test on Rails 1.2RC1
    git-svn-id: svn://rubyforge.org/var/svn/jpmobile/trunk/jpmobile@8 37be14ff-8d20-0410-9e9a-d2f6958099bb

--- test/helper.rb
+++ test/helper.rb
@@ -4,20 +4,15 @@ require 'action_controller'
 
 require 'init'
 
-class FakeCgi
+class FakeCgi < CGI
+  attr_accessor :stdinput, :stdoutput, :env_table
   def initialize(user_agent, env={})
-    @env_table = {"HTTP_USER_AGENT"=>user_agent}.update(env)
+    self.env_table = {"HTTP_USER_AGENT"=>user_agent,"QUERY_STRING"=>""}.update(env)
+    super()
   end
-  def query_string
-    @env_table["QUERY_STRING"] || ""
-  end
-  def params
-    CGIMethods.parse_query_parameters(query_string)
-  end
-  attr_reader :env_table
 end
 
 def request_with_ua(user_agent, env={})
   fake_cgi = FakeCgi.new(user_agent, env)
-  req = ActionController::CgiRequest.new(fake_cgi)
+  ActionController::CgiRequest.new(fake_cgi)
 end

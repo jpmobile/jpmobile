597338d8be78b40d4eb08729dee9447d35fe89a4
dara <dara@37be14ff-8d20-0410-9e9a-d2f6958099bb>
2007-05-18 10:26:51 +0000

    link_toとurl_forに分離
    git-svn-id: svn://rubyforge.org/var/svn/jpmobile/trunk/jpmobile@45 37be14ff-8d20-0410-9e9a-d2f6958099bb

--- lib/jpmobile/helpers.rb
+++ lib/jpmobile/helpers.rb
@@ -37,11 +37,16 @@ module Jpmobile
       return %{<a href="#{url_for(options)}" lcs>#{CGI.escapeHTML(str)}</a>}
     end
 
-    # DoCoMoでオープンiエリアを取得するためのリンクを返す。
-    def docomo_openiarea_link_to(str, options={})
+    # DoCoMoでオープンiエリアを取得するためのURLを返す。
+    def docomo_openiarea_url_for(options={})
       options = options.symbolize_keys
       options[:only_path] = false
-      return %{<a href="http://w1m.docomo.ne.jp/cp/iarea?ecode=OPENAREACODE&msn=OPENAREAKEY&nl=#{CGI.escape(url_for(options))}">#{CGI.escapeHTML(str)}</a>}
+      "http://w1m.docomo.ne.jp/cp/iarea?ecode=OPENAREACODE&msn=OPENAREAKEY&nl=#{CGI.escape(url_for(options))}"
+    end
+
+    # DoCoMoでオープンiエリアを取得するためのリンクを返す。
+    def docomo_openiarea_link_to(str, options={})
+      link_to_url(str, docomo_openiarea_url_for(options))
     end
 
     # DoCoMoで端末製造番号等を取得するためのリンクを返す。
@@ -51,20 +56,30 @@ module Jpmobile
       return %{<a href="#{url_for(options)}" utn>#{CGI.escapeHTML(str)}</a>}
     end
 
-    # au GPS位置情報を取得するためのリンクを返す。
-    def au_gps_link_to(str, options={})
+    # au GPS位置情報を取得するためのURLを返す。
+    def au_gps_url_for(options={})
       options = options.symbolize_keys
       options[:only_path] = false
       datum = (options.delete(:datum) || 0 ).to_i # 0:wgs84, 1:tokyo
       unit = (options.delete(:unit) || 0 ).to_i # 0:dms, 1:deg
-      return %{<a href="device:gpsone?url=#{CGI.escape(url_for(options))}&ver=1&datum=#{datum}&unit=#{unit}&acry=0&number=0">#{CGI.escapeHTML(str)}</a>}
+      "device:gpsone?url=#{CGI.escape(url_for(options))}&ver=1&datum=#{datum}&unit=#{unit}&acry=0&number=0"
+    end
+
+    # au GPS位置情報を取得するためのリンクを返す。
+    def au_gps_link_to(str, options={})
+      link_to_url(str, au_gps_url_for(options))
+    end
+
+    # au 簡易位置情報を取得するためのURLを返す。
+    def au_location_url_for(options={})
+      options = options.symbolize_keys
+      options[:only_path] = false
+      "device:location?url=#{CGI.escape(url_for(options))}"
     end
 
     # au 簡易位置情報を取得するためのリンクを返す。
     def au_location_link_to(str, options={})
-      options = options.symbolize_keys
-      options[:only_path] = false
-      return %{<a href="device:location?url=#{CGI.escape(url_for(options))}">#{CGI.escapeHTML(str)}</a>}
+      link_to_url(str, au_location_url_for(options))
     end
 
     # J-PHONE 位置情報 (基地局) を取得するためのリンクを返す。
@@ -74,19 +89,35 @@ module Jpmobile
       return %{<a z href="#{url_for(options)}">#{CGI.escapeHTML(str)}</a>}
     end
 
-    # Softbank(含むVodafone 3G)で位置情報を取得するためのリンクを返す。
-    def softbank_location_link_to(str,options={})
+    # Softbank(含むVodafone 3G)で位置情報を取得するためのURLを返す。
+    def softbank_location_url_for(options={})
       options = options.symbolize_keys
       options[:only_path] = false
       mode = options[:mode] || "auto"
-      return %{<a href="location:#{mode}?url=#{url_for(options)}">#{CGI.escapeHTML(str)}</a>}
+      "location:#{mode}?url=#{url_for(options)}"
+    end
+
+    # Softbank(含むVodafone 3G)で位置情報を取得するためのリンクを返す。
+    def softbank_location_link_to(str,options={})
+      link_to_url(str,softbank_location_url_for(options))
+    end
+
+    # Willcom 基地局位置情報を取得するためのURLを返す。
+    def willcom_location_url_for(options={})
+      options = options.symbolize_keys
+      options[:only_path] = false
+      "http://location.request/dummy.cgi?my=#{url_for(options)}&pos=$location"
     end
 
     # Willcom 基地局位置情報を取得するためのリンクを返す。
     def willcom_location_link_to(str,options={})
-      options = options.symbolize_keys
-      options[:only_path] = false
-      return %{<a href="http://location.request/dummy.cgi?my=#{url_for(options)}&pos=$location">#{CGI.escapeHTML(str)}</a>}
+      link_to_url(str, willcom_location_url_for(options))
+    end
+
+    private
+    # 外部へのリンク
+    def link_to_url(str, url)
+      %{<a href="#{url}">#{CGI.escapeHTML(str)}</a>}
     end
   end
 end

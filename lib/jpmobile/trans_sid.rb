182a64f6a63c88adeb4ec304de8bdfe0652f622d
dara <dara@37be14ff-8d20-0410-9e9a-d2f6958099bb>
2007-06-13 07:02:07 +0000

    リファクタリング
    git-svn-id: svn://rubyforge.org/var/svn/jpmobile/trunk/jpmobile@52 37be14ff-8d20-0410-9e9a-d2f6958099bb

--- lib/jpmobile/trans_sid.rb
+++ lib/jpmobile/trans_sid.rb
@@ -17,18 +17,29 @@ module Jpmobile::TransSid #:nodoc:
   end
 
   protected
+  # URLにsession_idを追加する。
   def default_url_options(options)
     return unless request # for test process 
-    return if transit_sid_mode == :none || (transit_sid_mode == :mobile && !request.mobile?)
-    session_key = request.session_options[:session_key] || '_session_id'
+    return unless apply_transit_sid?
     { session_key => session.session_id }
   end
   
   private
+  # session_keyを返す
+  def session_key
+    session_key = request.session_options[:session_key] || '_session_id'
+  end
+  # formにsession_idを追加する。
   def append_session_id_parameter
     return unless request # for test process 
-    return if transit_sid_mode == :none || (transit_sid_mode == :mobile && !request.mobile?)
-    session_key = request.session_options[:session_key] || '_session_id'
+    return unless apply_transit_sid?
     response.body.gsub!(%r{(</form>)}i, "<input type='hidden' name='#{CGI::escapeHTML session_key}' value='#{CGI::escapeHTML session.session_id}'>\\1")
+  end
+  # transit_sidを適用すべきか?
+  def apply_transit_sid?
+    return false if transit_sid_mode == :none
+    return true if transit_sid_mode == :always
+    return request.mobile? if transit_sid_mode == :mobile
+    return false
   end
 end

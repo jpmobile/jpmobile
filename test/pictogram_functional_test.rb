3240d4cc42e3b96cc9bf5f060bce42972c04325f
dara <dara@37be14ff-8d20-0410-9e9a-d2f6958099bb>
2007-06-19 04:48:10 +0000

    コントローラ込みの絵文字処理テストを追加。
    git-svn-id: svn://rubyforge.org/var/svn/jpmobile/trunk/jpmobile@70 37be14ff-8d20-0410-9e9a-d2f6958099bb

---
+++ test/pictogram_functional_test.rb
@@ -0,0 +1,37 @@
+require File.dirname(__FILE__)+'/helper'
+
+class PictogramTestController < ActionController::Base
+  mobile_filter
+  def docomo_cr
+    render :text=>"&#xE63E;"
+  end
+  def docomo_utf8
+    render :text=>"\xee\x98\xbe"
+  end
+  def docomo_query
+    @q = params[:q]
+    render :text=>@q
+  end
+end
+
+class PictogramFunctionalTest < Test::Unit::TestCase
+  def test_docomo
+    init PictogramTestController
+    
+    # PCから
+    get :docomo_cr
+    assert_equal "&#xE63E;", @response.body
+    get :docomo_utf8
+    assert_equal "\xee\x98\xbe", @response.body
+    
+    # DoCoMo携帯から
+    user_agent "DoCoMo/2.0 SH902i(c100;TB;W24H12)"
+    get :docomo_cr
+    assert_equal "\xf8\x9f", @response.body
+    get :docomo_utf8
+    assert_equal "\xf8\x9f", @response.body
+    get :docomo_query, :q=>"\xf8\x9f"
+    assert_equal "\xee\x98\xbe", assigns["q"]
+    assert_equal "\xf8\x9f", @response.body
+  end
+end

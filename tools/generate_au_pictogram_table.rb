f7820d0fa99df4b56a3e6e34c1cb453903a990b2
dara <dara@37be14ff-8d20-0410-9e9a-d2f6958099bb>
2007-06-19 03:16:40 +0000

    invertを使うように変更。
    git-svn-id: svn://rubyforge.org/var/svn/jpmobile/trunk/jpmobile@69 37be14ff-8d20-0410-9e9a-d2f6958099bb

--- tools/generate_au_pictogram_table.rb
+++ tools/generate_au_pictogram_table.rb
@@ -18,7 +18,7 @@ end
 open(File.dirname(__FILE__)+"/../lib/jpmobile/pictogram/au.rb","w") do |f|
   f.puts "Jpmobile::Pictogram::AU_SJIS_TO_UNICODE = {"
   table.each do |a|
-    f.puts "  0x%s => 0x%s," % [a[0],a[1]]
+    f.puts "  0x%s=>0x%s," % [a[0],a[1]]
   end
   f.puts "}.freeze"
   f.puts "Jpmobile::Pictogram::AU_UNICODE_TO_SJIS = Jpmobile::Pictogram::AU_SJIS_TO_UNICODE.invert.freeze"

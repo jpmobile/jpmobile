2ff160fcb9c40846df607a413a9fc0480bc2f6f6
dara <dara@37be14ff-8d20-0410-9e9a-d2f6958099bb>
2007-06-23 17:30:05 +0000

    正規表現のエンコーディングをnoneに指定
    git-svn-id: svn://rubyforge.org/var/svn/jpmobile/trunk/jpmobile@88 37be14ff-8d20-0410-9e9a-d2f6958099bb

--- lib/jpmobile/pictogram/z_combine.rb
+++ lib/jpmobile/pictogram/z_combine.rb
@@ -6,8 +6,8 @@ module Jpmobile
     SJIS_TO_UNICODE.freeze
     UNICODE_TO_SJIS = SJIS_TO_UNICODE.invert.freeze
 
-    SJIS_REGEXP = Regexp.union(*SJIS_TO_UNICODE.keys.map{|x| [x].pack('n')}).freeze
-    SOFTBANK_WEBCODE_REGEXP = Regexp.union(*SOFTBANK_WEBCODE_TO_UNICODE.keys.map{|x| "\x1b\x24#{x}\x0f"})
+    SJIS_REGEXP = Regexp.union(*([/(?!)/n]+SJIS_TO_UNICODE.keys.map{|x| [x].pack('n')})).freeze
+    SOFTBANK_WEBCODE_REGEXP = Regexp.union(*([/(?!)/n]+SOFTBANK_WEBCODE_TO_UNICODE.keys.map{|x| "\x1b\x24#{x}\x0f"}))
 
     PICTOGRAM_UNICODES = UNICODE_TO_SJIS.keys|SOFTBANK_UNICODE_TO_WEBCODE.keys.map{|k|k+0x1000}
     UTF8_REGEXP = Regexp.union(*PICTOGRAM_UNICODES.map{|x| [x].pack('U')}).freeze

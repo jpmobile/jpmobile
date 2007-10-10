0b0bc81a83d6b6d78586ec99e5d733062d64343d
dara <dara@37be14ff-8d20-0410-9e9a-d2f6958099bb>
2007-10-10 08:10:34 +0000

    SoftBankのIPアドレス帯域を変更
    git-svn-id: svn://rubyforge.org/var/svn/jpmobile/trunk/jpmobile@114 37be14ff-8d20-0410-9e9a-d2f6958099bb

--- lib/jpmobile/mobile/z_ip_addresses.rb
+++ lib/jpmobile/mobile/z_ip_addresses.rb
@@ -4,7 +4,7 @@
 # 2006/09現在
 # == SoftBank
 # http://developers.softbankmobile.co.jp/dp/tech_svc/web/ip.php
-# 2006/11/17現在
+# 2007/10/09現在
 
 #:enddoc:
 Jpmobile::Mobile::Docomo::IP_ADDRESSES = <<EOF
@@ -14,14 +14,14 @@ Jpmobile::Mobile::Docomo::IP_ADDRESSES = <<EOF
 EOF
 
 Jpmobile::Mobile::Softbank::IP_ADDRESSES=<<EOF
+123.108.236.0/24
+123.108.237.0/27
 202.179.204.0/24
-202.253.96.248/29
+202.253.96.224/27
 210.146.7.192/26
 210.146.60.192/26
 210.151.9.128/26
-210.169.130.112/29
-210.169.130.120/29
-210.169.176.0/24
+210.169.130.112/28
 210.175.1.128/25
 210.228.189.0/24
 211.8.159.128/25

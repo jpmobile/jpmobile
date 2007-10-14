85e9a64092074317b53cb3531e287f38ca9ae5c5
dara <dara@37be14ff-8d20-0410-9e9a-d2f6958099bb>
2007-10-14 13:08:27 +0000

    RSpecで書き換え
    git-svn-id: svn://rubyforge.org/var/svn/jpmobile/trunk/jpmobile@115 37be14ff-8d20-0410-9e9a-d2f6958099bb

--- test/firefox_test.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__)+'/helper'
-
-class JpmobileTest < Test::Unit::TestCase
-  def test_ie
-    req = request_with_ua("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)")
-    assert_equal(false, req.mobile?)
-  end
-end

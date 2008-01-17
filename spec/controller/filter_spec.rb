f40d5219344dbc05620d50c9a11acc4dbf76a1dc
dara <dara@37be14ff-8d20-0410-9e9a-d2f6958099bb>
2008-01-17 12:12:12 +0000

    テストをRSpecに書き換え
    git-svn-id: svn://rubyforge.org/var/svn/jpmobile/trunk/jpmobile@141 37be14ff-8d20-0410-9e9a-d2f6958099bb

--- spec/controller/filter_spec.rb
+++ spec/controller/filter_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "文字コードフィルタが動作しているとき", :shared => true do
-  it "はhtml以外は変換しない" do
+  it "はhtml以外は変換しないこと" do
     get :rawdata
     response.charset.should be_nil
     response.body.should == "アブラカダブラ"
@@ -14,9 +14,18 @@ describe "Shift_JISで通信する端末との通信", :shared => true do
     response.body.should == "アブラカダブラ".tosjis
     response.charset.should == "Shift_JIS"
   end
+  it "はxhtmlでもShift_JISで携帯に送出されること" do
+    get :abracadabra_xhtml_utf8
+    response.body.should == "アブラカダブラ".tosjis
+    response.charset.should == "Shift_JIS"
+  end
   it "はShift_JISで渡されたパラメタがparamsにUTF-8に変換されて格納されること" do
     get :index, :q => "アブラカダブラ".tosjis
     assigns[:q].should == "アブラカダブラ"
+  end
+  it "は半角カナのparamsを変換しないこと" do
+    get :index, :q => "\261\314\336\327\266\300\336\314\336\327" # アブラカダブラ半角,SJIS
+    assigns[:q].should == "ｱﾌﾞﾗｶﾀﾞﾌﾞﾗ"
   end
   it_should_behave_like "文字コードフィルタが動作しているとき"
 end
@@ -27,16 +36,80 @@ describe "UTF-8で通信する端末との通信", :shared => true do
     response.body.should == "アブラカダブラ"
     response.charset.should == "utf-8"
   end
+  it "はxhtmlでもUTF-8で携帯に送出されること" do
+    get :abracadabra_xhtml_utf8
+    response.body.should == "アブラカダブラ"
+    response.charset.should == "utf-8"
+  end
   it "はparamsにUTF-8のまま格納されること" do
     get :index, :q => "アブラカダブラ"
+    assigns[:q].should == "アブラカダブラ"
+  end
+  it "は半角カナのparamsを変換しないこと" do
+    get :index, :q => "ｱﾌﾞﾗｶﾀﾞﾌﾞﾗ"
+    assigns[:q].should == "ｱﾌﾞﾗｶﾀﾞﾌﾞﾗ"
+  end
+  it_should_behave_like "文字コードフィルタが動作しているとき"
+end
+
+describe "Shift_JISで通信する端末との通信(半角変換付き)", :shared => true do
+  it "は半角に変換されShift_JISで携帯に送出されること" do
+    get :abracadabra_utf8
+    response.body.should == "\261\314\336\327\266\300\336\314\336\327" # アブラカダブラ半角,SJIS
+    response.charset.should == "Shift_JIS"
+  end
+  it "はShift_JISで渡されたパラメタがparamsにUTF-8に変換されて格納されること" do
+    get :index, :q => "アブラカダブラ".tosjis
+    assigns[:q].should == "アブラカダブラ"
+  end
+  it "は半角Shift_JISで渡されたパラメタがparamsに全角UTF-8に変換されて格納されること" do
+    get :index, :q => "\261\314\336\327\266\300\336\314\336\327" # アブラカダブラ半角,SJIS
     assigns[:q].should == "アブラカダブラ"
   end
   it_should_behave_like "文字コードフィルタが動作しているとき"
 end
 
+describe "UTF-8で通信する端末との通信(半角変換付き)", :shared => true do
+  it "はUTF-8半角で携帯に送出されること" do
+    get :abracadabra_utf8
+    response.body.should == "ｱﾌﾞﾗｶﾀﾞﾌﾞﾗ"
+    response.charset.should == "utf-8"
+  end
+  it "はparamsにUTF-8のまま格納されること" do
+    get :index, :q => "アブラカダブラ"
+    assigns[:q].should == "アブラカダブラ"
+  end
+  it "は半角で渡されたparamsを全角に変換して格納すること" do
+    get :index, :q => "ｱﾌﾞﾗｶﾀﾞﾌﾞﾗ"
+    assigns[:q].should == "アブラカダブラ"
+  end
+  it_should_behave_like "文字コードフィルタが動作しているとき"
+end
+
+#
+# PCからのアクセス
+#
+describe FilterController, "PCからのアクセス" do
+  it_should_behave_like "UTF-8で通信する端末との通信"
+end
+
+describe HankakuFilterController, "PCからのアクセス" do
+  it_should_behave_like "UTF-8で通信する端末との通信"
+end
+
+#
+# 携帯からのアクセス
+#
 describe FilterController, "DoCoMo SH902i からのアクセス" do
   before do
     request.user_agent = "DoCoMo/2.0 SH902i(c100;TB;W24H12)"
+  end
+  it_should_behave_like "Shift_JISで通信する端末との通信"
+end
+
+describe FilterController, "au CA32 からのアクセス" do
+  before do
+    request.user_agent = "KDDI-CA32 UP.Browser/6.2.0.7.3.129 (GUI) MMP/2.0"
   end
   it_should_behave_like "Shift_JISで通信する端末との通信"
 end
@@ -60,4 +133,21 @@ describe FilterController, "SoftBank 910T からのアクセス" do
     request.user_agent = "SoftBank/1.0/910T/TJ001/SN000000000000000 Browser/NetFront/3.3 Profile/MIDP-2.0 Configuration/CLDC-1.1"
   end
   it_should_behave_like "UTF-8で通信する端末との通信"
+end
+
+#
+# 半角フィルタ
+#
+describe HankakuFilterController, "DoCoMo SH902i からのアクセス" do
+  before do
+    request.user_agent = "DoCoMo/2.0 SH902i(c100;TB;W24H12)"
+  end
+  it_should_behave_like "Shift_JISで通信する端末との通信(半角変換付き)"
+end
+
+describe HankakuFilterController, "SoftBank 910T からのアクセス" do
+  before do
+    request.user_agent = "SoftBank/1.0/910T/TJ001/SN000000000000000 Browser/NetFront/3.3 Profile/MIDP-2.0 Configuration/CLDC-1.1"
+  end
+  it_should_behave_like "UTF-8で通信する端末との通信(半角変換付き)"
 end
--- spec_resources/controllers/filter_controller.rb
+++ spec_resources/controllers/filter_controller.rb
@@ -1,6 +1,9 @@
-class FilterController < ApplicationController
-  mobile_filter
+class FilterControllerBase < ApplicationController
   def abracadabra_utf8
+    render :text => "アブラカダブラ"
+  end
+  def abracadabra_xhtml_utf8
+    response.content_type = "application/xhtml+xml"
     render :text => "アブラカダブラ"
   end
   def index
@@ -9,4 +12,12 @@ class FilterController < ApplicationController
   def rawdata
     send_data "アブラカダブラ", :type => 'application/octet-stream'
   end
+end
+
+class FilterController < FilterControllerBase
+  mobile_filter
+end
+
+class HankakuFilterController < FilterControllerBase
+  mobile_filter :hankaku => true
 end

6b45ebbfa7dc0ef95eaa6e5415c2586c5730455f
dara <dara@37be14ff-8d20-0410-9e9a-d2f6958099bb>
2008-02-20 11:22:38 +0000

    [#32] に対応。
    git-svn-id: svn://rubyforge.org/var/svn/jpmobile/trunk/jpmobile@146 37be14ff-8d20-0410-9e9a-d2f6958099bb

--- lib/jpmobile/hook_action_controller.rb
+++ lib/jpmobile/hook_action_controller.rb
@@ -1,9 +1,9 @@
 class ActionController::Base
   before_filter :gettext_force_ja_for_mobile
-  # gettextが組み込まれている場合、携帯電話からのアクセスをjaロケール強制する。
+  # gettextが組み込まれている場合、携帯電話からのアクセスをjaロケールに強制する。
   def gettext_force_ja_for_mobile
     begin
-      ::GetText.locale = request.mobile? ? 'ja' : nil
+      ::GetText.locale = 'ja' if request.mobile?
     rescue NameError
     end
   end

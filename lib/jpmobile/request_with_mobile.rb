b01a86cd18296661a84bdceac14374dd86d1a244
Keiji, Yoshimi <walf443@gmail.com>
2008-06-28 22:54:45 +0900

    Jpmobile::RequestWithMobileはRailsに依存してなくても良いのでファイルを分ける

---
+++ lib/jpmobile/request_with_mobile.rb
@@ -0,0 +1,32 @@
+# jpmobile の各機能を提供するモジュール
+# envメソッドと、parameter あるいは params メソッドが実装されている必要がある。
+# 今のところはRack::RequestとActionController::AbstractRequestに対応しているはず。
+
+module Jpmobile
+  module RequestWithMobile
+    # 環境変数 HTTP_USER_AGENT を返す。
+    def user_agent
+      env['HTTP_USER_AGENT']
+    end
+    # 環境変数 HTTP_USER_AGENT を設定する。
+    def user_agent=(str)
+      self.env["HTTP_USER_AGENT"] = str
+    end
+
+    # 携帯電話からであれば +true+を、そうでなければ +false+ を返す。
+    def mobile?
+      mobile != nil
+    end
+
+    # 携帯電話の機種に応じて Mobile::xxx を返す。
+    # 携帯電話でない場合はnilを返す。
+    def mobile
+      Jpmobile::Mobile.constants.each do |const|
+        c = Jpmobile::Mobile.const_get(const)
+        return c.new(self) if c::USER_AGENT_REGEXP && user_agent =~ c::USER_AGENT_REGEXP
+      end
+      nil
+    end
+  end
+end
+

3dbdaf2e6db4a63721a91c0e3e99f95ebc9a072a
Yohji Shidara <dara@shidara.net>
2008-07-02 21:43:20 +0900

    [#40] Rails 2.1のpartial対応における#40の不都合 に対応
    あまり綺麗なやり方ではないけれど・・・

--- lib/jpmobile/hook_action_view.rb
+++ lib/jpmobile/hook_action_view.rb
@@ -1,6 +1,6 @@
 # = viewの自動切り替え
 #
-# Rails 2.0.1 対応 http://d.hatena.ne.jp/kusakari/20080620/1213931903
+# Rails 2.1.0 対応 http://d.hatena.ne.jp/kusakari/20080620/1213931903
 # thanks to id:kusakari
 #
 #:stopdoc:
@@ -17,6 +17,7 @@ ActionView::Base.class_eval { include Jpmobile::Helpers }
 # の順にテンプレートが検索される。
 # BUG: 現状、上記の例では index.rhtml が存在しない場合に振り分けが行われない
 # (ダミーファイルを置くことで回避可能)。
+
 class ActionView::Base #:nodoc:
   alias render_file_without_jpmobile render_file #:nodoc:
   alias render_partial_without_jpmobile render_partial #:nodoc:
@@ -35,17 +36,29 @@ class ActionView::Base #:nodoc:
 
   def mobile_template_path(template_path, partial=false)
     if controller.is_a?(ActionController::Base) && m = controller.request.mobile
-      vals = []
+      candidates = []
       c = m.class
       while c != Jpmobile::Mobile::AbstractMobile
-        vals << "mobile_"+c.to_s.split(/::/).last.downcase
+        candidates << "mobile_"+c.to_s.split(/::/).last.downcase
         c = c.superclass
       end
-      vals << "mobile"
+      candidates << "mobile"
 
-      vals.each do |v|
+      candidates.each do |v|
         mobile_path = "#{template_path}_#{v}"
-        full_path = partial ? "#{self.controller.class.controller_path}/_#{mobile_path}" : mobile_path
+        if partial
+          # ActionView::PartialTemplate#extract_partial_name_and_path の動作を模倣
+          if mobile_path.include?('/')
+            path = File.dirname(mobile_path)
+            partial_name = File.basename(mobile_path)
+          else
+            path = self.controller.class.controller_path
+            partial_name = mobile_path
+          end
+          full_path = File.join(path, "_#{partial_name}")
+        else
+          full_path = mobile_path
+        end
         if finder.file_exists?(full_path)
           return mobile_path
         end
@@ -54,4 +67,3 @@ class ActionView::Base #:nodoc:
     return nil
   end
 end
-

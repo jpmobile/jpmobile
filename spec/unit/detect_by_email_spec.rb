17832636b07da9b6a16a12c0599caaf2cd9e0905
Keiji, Yoshimi <walf443@gmail.com>
2008-09-27 02:04:38 +0900

    Jpmobile::Emailをautoloadさせるように変更 Jpmobile::Email.career_by_emailという名前はemailがかぶっていて冗長なので、detectで十分かと思いました Jpmobile::Emailのテストはspec/unitに移行 Jpmobile::Mobile.constantsではなくJpmobile::Mobile.careersを使うように 上記の影響で"pdx.ne.jp"の際に,Ddipocketと判定するようになってしまったのでDdipocketとは判定しないように変更

---
+++ spec/unit/detect_by_email_spec.rb
@@ -0,0 +1,22 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe 'Jpmobile::Email' do
+
+  Spec::Fixture::Base.new self, :email_addr => :career do
+
+    it '#detect should return :career when take :email_addr as UserAgent' do |email_addr, career|
+      Jpmobile::Email.detect(email_addr).class.should == career
+    end
+
+    set_fixtures([
+      ['test@docomo.ne.jp'      => Jpmobile::Mobile::Docomo ],
+      ['a(--)l@ezweb.ne.jp'     => Jpmobile::Mobile::Au ],
+      ['dadaea@pdx.ne.jp'       => Jpmobile::Mobile::Willcom  ],
+      ['xxxe@dj.pdx.ne.jp'      => Jpmobile::Mobile::Willcom  ],
+      ['oeeikx@softbank.ne.jp'  => Jpmobile::Mobile::Softbank ],
+      ['eaae@disney.ne.jp'      => Jpmobile::Mobile::Softbank ],
+      ['iiiaa@r.vodafone.ne.jp' => Jpmobile::Mobile::Vodafone ],
+      ['aaaaa.aaaa@jp-h.ne.jp' => Jpmobile::Mobile::Jphone    ],
+    ])
+  end.run
+end

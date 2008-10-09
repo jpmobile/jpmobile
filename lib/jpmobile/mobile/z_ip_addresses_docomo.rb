e8fc3dcbd5fb8621b2d80ffd8e4c163618d5bf5e
Yohji Shidara <dara@shidara.net>
2008-10-10 00:06:34 +0900

    DoCoMoのIPアドレスを追加

--- lib/jpmobile/mobile/z_ip_addresses_docomo.rb
+++ lib/jpmobile/mobile/z_ip_addresses_docomo.rb
@@ -1,10 +1,12 @@
 # = IPアドレス帯域テーブル(手動更新分)
 # == DoCoMo
 # http://www.nttdocomo.co.jp/service/imode/make/content/ip/index.html
-# 2006/09現在
+# 2008/09現在
 Jpmobile::Mobile::Docomo::IP_ADDRESSES = %w(
 210.153.84.0/24
 210.136.161.0/24
 210.153.86.0/24
+124.146.174.0/24
+124.146.175.0/24
 ).map {|ip| IPAddr.new(ip) }
 

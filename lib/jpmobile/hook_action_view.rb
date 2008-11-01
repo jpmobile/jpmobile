47aa7fd0ba73e0db62ce55028055b478d3bbe3ce
YAMAGUCHI, Seiji <valda68k@gmail.com>
2008-11-01 23:25:59 +0900

    Rails 2.2 RC1 (actionpack 2.2.0) へ対応

--- lib/jpmobile/hook_action_view.rb
+++ lib/jpmobile/hook_action_view.rb
@@ -19,19 +19,43 @@ ActionView::Base.class_eval { include Jpmobile::Helpers }
 # (ダミーファイルを置くことで回避可能)。
 
 class ActionView::Base #:nodoc:
-  alias render_file_without_jpmobile render_file #:nodoc:
-  alias render_partial_without_jpmobile render_partial #:nodoc:
+  require 'action_pack/version'
 
-  def render_file(template_path, use_full_path = true, local_assigns = {})
-    mobile_path = mobile_template_path(template_path)
-    return mobile_path.nil? ? render_file_without_jpmobile(template_path, use_full_path, local_assigns) :
-                              render_file_without_jpmobile(mobile_path, use_full_path, local_assigns)
-  end
+  if ::ActionPack::VERSION::MAJOR >=2 and ::ActionPack::VERSION::MINOR >= 2
+    ### Rails 2.2 or higher
+    alias _pick_template_without_jpmobile _pick_template #:nodoc:
+    alias render_partial_without_jpmobile render_partial #:nodoc:
 
-  def render_partial(partial_path, object_assigns = nil, local_assigns = {}) #:nodoc:
-    mobile_path = mobile_template_path(partial_path, true) if partial_path.class === "String"
-    return mobile_path.nil? ? render_partial_without_jpmobile(partial_path, object_assigns, local_assigns) :
-    render_partial_without_jpmobile(mobile_path, object_assigns, local_assigns)
+    def _pick_template(template_path)
+      mobile_path = mobile_template_path(template_path)
+      return mobile_path.nil? ? _pick_template_without_jpmobile(template_path) :
+                                _pick_template_without_jpmobile(mobile_path)
+    end
+
+    def render_partial(options = {}) #:nodoc:
+      case partial_path = options[:partial]
+      when String, Symbol, NilClass
+        mobile_path = mobile_template_path(partial_path, true)
+        options = options.merge(:partial => mobile_path) if mobile_path
+      end
+      render_partial_without_jpmobile(options)
+    end
+  else
+    ### Rails 2.1 or lower
+    alias render_file_without_jpmobile render_file #:nodoc:
+    alias render_partial_without_jpmobile render_partial #:nodoc:
+
+    def render_file(template_path, use_full_path = true, local_assigns = {})
+      mobile_path = mobile_template_path(template_path)
+      return mobile_path.nil? ? render_file_without_jpmobile(template_path, use_full_path, local_assigns) :
+                                render_file_without_jpmobile(mobile_path, use_full_path, local_assigns)
+    end
+
+    def render_partial(partial_path, object_assigns = nil, local_assigns = {}) #:nodoc:
+      mobile_path = mobile_template_path(partial_path, true) if partial_path.class === "String"
+      return mobile_path.nil? ? render_partial_without_jpmobile(partial_path, object_assigns, local_assigns) :
+                                render_partial_without_jpmobile(mobile_path, object_assigns, local_assigns)
+    end
   end
 
   def mobile_template_candidates
@@ -69,11 +93,25 @@ class ActionView::Base #:nodoc:
         else
           full_path = mpath
         end
-        if finder.file_exists?(full_path)
+        if template_exists?(full_path)
           return mpath
         end
       end
     end
     return nil
+  end
+
+  if ::ActionPack::VERSION::MAJOR >=2 and ::ActionPack::VERSION::MINOR >= 2
+    ### Rails 2.2 or higher
+    def template_exists?(template_name)
+      send(:_pick_template_without_jpmobile, template_name) ? true : false
+    rescue ActionView::MissingTemplate
+      false
+    end
+  else
+    ### Rails 2.1 or lower
+    def template_exists?(template_name)
+      finder.file_exists?(template_name)
+    end
   end
 end

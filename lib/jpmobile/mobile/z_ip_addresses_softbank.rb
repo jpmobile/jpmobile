ac03e29d72cf8ae8cad8a7563e5002690eca445d
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2009-08-06 14:28:51 +0900

    Update ip-adddresses.
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- lib/jpmobile/mobile/z_ip_addresses_softbank.rb
+++ lib/jpmobile/mobile/z_ip_addresses_softbank.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # = IPアドレス帯域テーブル(手動更新分)
 # == SoftBank
 # http://developers.softbankmobile.co.jp/dp/tech_svc/web/ip.php
@@ -10,8 +11,7 @@ Jpmobile::Mobile::Softbank::IP_ADDRESSES = %w(
 210.146.7.192/26
 210.146.60.192/26
 210.151.9.128/26
-210.169.130.112/28
 210.175.1.128/25
-210.228.189.0/24
 211.8.159.128/25
+210.175.1.128/25
 ).map {|ip| IPAddr.new(ip) }

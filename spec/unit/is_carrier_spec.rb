37e5a8377cae50224da11dca3bf1e54a75177ad5
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2010-06-14 18:11:32 +0900

    Fix initializer parameter, and remove J-Phone
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- spec/unit/is_carrier_spec.rb
+++ spec/unit/is_carrier_spec.rb
@@ -13,7 +13,7 @@ describe 'Jpmobile::Mobile' do
     })
 
     it ":klass#:carrier should be return :expected" do |input, expected|
-      input[:klass].new({}).__send__(input[:carrier]).should == expected
+      input[:klass].new({}, {}).__send__(input[:carrier]).should == expected
     end
 
     set_fixtures([
@@ -21,7 +21,6 @@ describe 'Jpmobile::Mobile' do
       [ %w[ Docomo au        ] => false ],
       [ %w[ Docomo softbank  ] => false ],
       [ %w[ Docomo vodafone  ] => false ],
-      [ %w[ Docomo jphone    ] => false ],
       [ %w[ Docomo emobile   ] => false ],
       [ %w[ Docomo willcom   ] => false ],
       [ %w[ Docomo ddipocket ] => false ],
@@ -30,7 +29,6 @@ describe 'Jpmobile::Mobile' do
       [ %w[ Au au        ] => true ],
       [ %w[ Au softbank  ] => false ],
       [ %w[ Au vodafone  ] => false ],
-      [ %w[ Au jphone    ] => false ],
       [ %w[ Au emobile   ] => false ],
       [ %w[ Au willcom   ] => false ],
       [ %w[ Au ddipocket ] => false ],
@@ -39,7 +37,6 @@ describe 'Jpmobile::Mobile' do
       [ %w[ Softbank au        ] => false ],
       [ %w[ Softbank softbank  ] => true ],
       [ %w[ Softbank vodafone  ] => false ],
-      [ %w[ Softbank jphone    ] => false ],
       [ %w[ Softbank emobile   ] => false ],
       [ %w[ Softbank willcom   ] => false ],
       [ %w[ Softbank ddipocket ] => false ],
@@ -48,7 +45,6 @@ describe 'Jpmobile::Mobile' do
       [ %w[ Vodafone au        ] => false ],
       [ %w[ Vodafone softbank  ] => true  ],
       [ %w[ Vodafone vodafone  ] => true  ],
-      [ %w[ Vodafone jphone    ] => false ],
       [ %w[ Vodafone emobile   ] => false ],
       [ %w[ Vodafone willcom   ] => false ],
       [ %w[ Vodafone ddipocket ] => false ],
@@ -57,7 +53,6 @@ describe 'Jpmobile::Mobile' do
       [ %w[ Emobile au        ] => false ],
       [ %w[ Emobile softbank  ] => false ],
       [ %w[ Emobile vodafone  ] => false ],
-      [ %w[ Emobile jphone    ] => false ],
       [ %w[ Emobile emobile   ] => true  ],
       [ %w[ Emobile willcom   ] => false ],
       [ %w[ Emobile ddipocket ] => false ],
@@ -66,7 +61,6 @@ describe 'Jpmobile::Mobile' do
       [ %w[ Willcom au        ] => false ],
       [ %w[ Willcom softbank  ] => false ],
       [ %w[ Willcom vodafone  ] => false ],
-      [ %w[ Willcom jphone    ] => false ],
       [ %w[ Willcom emobile   ] => false ],
       [ %w[ Willcom willcom   ] => true  ],
       [ %w[ Willcom ddipocket ] => false ],
@@ -75,7 +69,6 @@ describe 'Jpmobile::Mobile' do
       [ %w[ Ddipocket au        ] => false ],
       [ %w[ Ddipocket softbank  ] => false ],
       [ %w[ Ddipocket vodafone  ] => false ],
-      [ %w[ Ddipocket jphone    ] => false ],
       [ %w[ Ddipocket emobile   ] => false ],
       [ %w[ Ddipocket willcom   ] => true  ],
       [ %w[ Ddipocket ddipocket ] => true  ],

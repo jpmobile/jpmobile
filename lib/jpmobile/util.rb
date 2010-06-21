c939b3e8a29f045b74ad8e1a63e057c51661e5ea
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2010-06-21 18:20:36 +0900

    Revert util.rb in 16b8f769b28a5fdc1478b08cbceadbfb04c2434f
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- lib/jpmobile/util.rb
+++ lib/jpmobile/util.rb
@@ -2,16 +2,12 @@ require 'tempfile'
 module Jpmobile
   module Util
     module_function
-
     def deep_apply(obj, &proc)
       case obj
       when Hash
-        new_obj = {}
         obj.each_pair do |key, value|
-          new_key = deep_apply(key, &proc)
-          new_obj[new_key] = deep_apply(value, &proc)
+          obj[key] = deep_apply(value, &proc)
         end
-        new_obj
       when Array
         obj.collect!{|value| deep_apply(value, &proc)}
       when NilClass, TrueClass, FalseClass, Tempfile, StringIO

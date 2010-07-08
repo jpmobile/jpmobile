59a0a2b3b4af274dbba0fe8a9c4df4eba29eeeee
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2010-07-08 14:45:38 +0900

    Fix methods correctly
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- test/sinatra/test/filter_test.rb
+++ test/sinatra/test/filter_test.rb
@@ -15,25 +15,25 @@ class SinatraOnJpmobile < Test::Unit::TestCase
     SinatraTestHelper.instance.last_app
   end
 
-  # def test_not_convert_pc_get
-  #   get '/', {:g => Jpmobile::Util.sjis("万葉")}, {}
-  #   assert_equal last_response.body, "万葉"
-  # end
+  def test_not_convert_pc_get
+    get '/', {:g => Jpmobile::Util.sjis("万葉")}, {}
+    assert_equal last_response.body, "万葉"
+  end
 
-  # def test_not_convert_pc_post
-  #   post '/', {:p => Jpmobile::Util.utf8("けーたい")}, {}
-  #   assert_equal last_response.body, "けーたい"
-  # end
+  def test_not_convert_pc_post
+    post '/', {:p => Jpmobile::Util.utf8("けーたい")}, {}
+    assert_equal last_response.body, "けーたい"
+  end
 
   def test_docomo_get_convert_to_utf8
-    get '/', {:g => to_sjis("万葉")}, {"HTTP_USER_AGENT" => "DoCoMo/2.0 SH902i(c100;TB;W24H12)"}
+    get '/', {:g => utf8_to_sjis("万葉")}, {"HTTP_USER_AGENT" => "DoCoMo/2.0 SH902i(c100;TB;W24H12)"}
     assert_equal last_app.assigns(:g), "万葉"
-    assert_equal last_response.body, to_sjis("万葉")
+    assert_equal last_response.body, utf8_to_sjis("万葉")
   end
 
   def test_docomo_post_convert_to_utf8
-    post '/', {:p => to_sjis("けーたい")}, {"HTTP_USER_AGENT" => "DoCoMo/2.0 SH902i(c100;TB;W24H12)"}
+    post '/', {:p => utf8_to_sjis("けーたい")}, {"HTTP_USER_AGENT" => "DoCoMo/2.0 SH902i(c100;TB;W24H12)"}
     assert_equal last_app.assigns(:p), "けーたい"
-    assert_equal last_response.body, to_sjis("けーたい")
+    assert_equal last_response.body, utf8_to_sjis("けーたい")
   end
 end

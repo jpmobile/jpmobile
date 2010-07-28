ab3d18211b634ec3ba3fa47a8c7b71962bf754b3
SHIDARA Yoji <dara@shidara.net>
2010-07-28 19:13:10 +0900

    adjust loading order

--- lib/jpmobile/rails.rb
+++ lib/jpmobile/rails.rb
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-ActiveSupport.on_load(:before_configuration) do
+ActiveSupport.on_load(:action_controller) do
   require 'jpmobile/docomo_guid'
   require 'jpmobile/filter'
   require 'jpmobile/helpers'
   require 'jpmobile/hook_action_controller'
   require 'jpmobile/hook_action_view'
   require 'jpmobile/trans_sid'
+end
 
+ActiveSupport.on_load(:before_configuration) do
   # MobileCarrierのみデフォルトで有効
   ::Rails.application.middleware.insert_before('ActionDispatch::ParamsParser', Jpmobile::Rack::MobileCarrier)
-
   module Rails
     class Application
       class Configuration

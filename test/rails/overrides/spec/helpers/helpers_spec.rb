7345e5f186098a2789fdcecf80c2f6198daf9394
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2010-12-17 10:33:56 +0900

    Fix spec corresponding to 0d97031b5962dcea2c000b54d48f63827716a381

--- test/rails/overrides/spec/helpers/helpers_spec.rb
+++ test/rails/overrides/spec/helpers/helpers_spec.rb
@@ -10,6 +10,6 @@ describe Jpmobile::Helpers do
 
   it "softbank_location_link_to がリンク先にパラメータを含んでいても正常に動作すること" do
     # http://d.hatena.ne.jp/mizincogrammer/20090123/1232702067
-    softbank_location_link_to("STRING", :controller => "filter", :action => "rawdata", :p => "param").should == %{<a href="location:auto?url=http://test.host/filter/rawdata&p=param">STRING</a>}
+    softbank_location_link_to("STRING", :controller => "filter", :action => "rawdata", :p => "param").should == %{<a href="location:auto?url=http://test.host/filter/rawdata&amp;p=param">STRING</a>}
   end
 end

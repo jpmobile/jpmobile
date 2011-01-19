f7b2ff0b49b90f4a40c5c889d689f1f9a221c4fe
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2011-01-19 10:30:51 +0900

    Fix spec

--- spec/unit/receive_mail_spec.rb
+++ spec/unit/receive_mail_spec.rb
@@ -19,7 +19,7 @@ describe "Jpmobile::Mail#receive" do
       @mail.subject.should == "タイトルの長いメールの場合の対処を実装するためのテストケースとしてのメールに含まれている件名であるサブジェクト部分"
     end
 
-    it "subject should be parsed correctly" do
+    it "body should be parsed correctly" do
       @mail.body.to_s.should == "本文です"
     end
   end
@@ -33,25 +33,23 @@ describe "Jpmobile::Mail#receive" do
       @mail.subject.should == "タイトルの長いメールの場合の対処を実装するためのテストケースとしてのメールに含まれている件名であるサブジェクト部分"
     end
 
-    it "subject should be parsed correctly" do
+    it "body should be parsed correctly" do
       @mail.body.parts.size.should == 2
       @mail.body.parts.first.body.to_s.should == "本文です"
     end
   end
 
- describe "Docomo" do
+  describe "Docomo" do
     before(:each) do
       @mail = Mail.new(open(File.join(File.expand_path(File.dirname(__FILE__)), "email-fixtures/docomo-emoji.eml")).read)
     end
 
-    context "to_s" do
-      it "should contain encoded subject" do
-        @mail.subject.should == "題名&#xe676;"
-      end
+    it "subject should be parsed correctly" do
+      @mail.subject.should == "題名&#xe676;"
+    end
 
-      it "should contain encoded body" do
-        @mail.body.to_s.should == "本文&#xe6e2;\nFor docomo"
-      end
+    it "body should be parsed correctly" do
+      @mail.body.to_s.should == "本文&#xe6e2;\nFor docomo"
     end
   end
 
@@ -60,14 +58,12 @@ describe "Jpmobile::Mail#receive" do
       @mail = Mail.new(open(File.join(File.expand_path(File.dirname(__FILE__)), "email-fixtures/au-emoji.eml")).read)
     end
 
-    context "to_s" do
-      it "should contain encoded subject" do
-        @mail.subject.should == "題名&#xe503;"
-      end
+    it "subject should be parsed correctly" do
+      @mail.subject.should == "題名&#xe503;"
+    end
 
-      it "should contain encoded body" do
-        @mail.body.to_s.should == "本文&#xe522;\nFor au"
-      end
+    it "body should be parsed correctly" do
+      @mail.body.to_s.should == "本文&#xe522;\nFor au"
     end
   end
 
@@ -76,14 +72,14 @@ describe "Jpmobile::Mail#receive" do
       @mail = Mail.new(open(File.join(File.expand_path(File.dirname(__FILE__)), "email-fixtures/softbank-emoji.eml")).read)
     end
 
-    context "to_s" do
-      it "should contain encoded subject" do
-        @mail.subject.should == "題名&#xf03c;"
-      end
+    it "subject should be parsed correctly" do
+      @mail.subject.should == "題名&#xf03c;"
+    end
 
-      it "should contain encoded body" do
-        @mail.body.to_s.should == "本文&#xf21c;\nFor softbank"
-      end
+    it "body should be parsed correctly" do
+      @mail.body.to_s.should == "本文&#xf21c;\nFor softbank"
+    end
+  end
     end
   end
 end

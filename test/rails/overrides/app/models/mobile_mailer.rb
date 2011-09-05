23b64e44aa31e5a7ce4eaf0cf683d137337a4e5e
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2011-09-05 11:00:06 +0900

    Use default_params if :to is no specified. Closes gh-26

--- test/rails/overrides/app/models/mobile_mailer.rb
+++ test/rails/overrides/app/models/mobile_mailer.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 class MobileMailer < Jpmobile::Mailer::Base
   default :from => "info@jp.mobile"
+  default :to   => "info@jp.mobile"
 
   def view_selection(to_mail, subject_text, text)
     @text = text
@@ -15,5 +16,10 @@ class MobileMailer < Jpmobile::Mailer::Base
     @html = html
     @text = text
     mail(:to => to_mail, :subject => subject_text)
+  end
+
+  def default_to_mail(subject_text, text)
+    @text = text
+    mail(:subject => subject_text)
   end
 end

dbf076551aa9c63a72da194e86a412556f2ed8e2
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2011-12-21 18:50:44 +0900

    Modify virtual_path in ActionView::Template for TESTING

--- lib/jpmobile/resolver.rb
+++ lib/jpmobile/resolver.rb
@@ -8,5 +8,26 @@ module Jpmobile
       super(path, pattern || DEFAULT_PATTERN)
       @path = File.expand_path(path)
     end
+
+    private
+
+    def query(path, details, formats)
+      query = build_query(path, details)
+      templates = []
+      sanitizer = Hash.new { |h,k| h[k] = Dir["#{File.dirname(k)}/*"] }
+
+      Dir[query].each do |p|
+        next if File.directory?(p) || !sanitizer[p].include?(p)
+
+        handler, format = extract_handler_and_format(p, formats)
+        contents = File.open(p, "rb") { |io| io.read }
+        variant = p.match(/.+#{path}(.+)\.#{format.to_sym.to_s}.*$/) ? $1 : ''
+
+        templates << ActionView::Template.new(contents, File.expand_path(p), handler,
+          :virtual_path => path.name + variant, :format => format, :updated_at => mtime(p))
+      end
+
+      templates
+    end
   end
 end

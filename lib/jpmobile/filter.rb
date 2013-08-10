e682bce03daa5c3b46517cdf258d789816664751
akicho8 <akicho8@gmail.com>
2013-08-10 21:38:27 +0900

    HankakuFilter optimize

--- lib/jpmobile/filter.rb
+++ lib/jpmobile/filter.rb
@@ -47,14 +47,15 @@ module Jpmobile
       end
     end
 
-    @@internal = %w(ガ ギ グ ゲ ゴ ザ ジ ズ ゼ ゾ ダ ヂ ヅ デ ド バ ビ ブ ベ ボ パ ピ プ ペ ポ ヴ ア イ ウ エ オ カ キ ク ケ コ サ シ ス セ ソ タ チ ツ テ ト ナ ニ ヌ ネ ノ ハ ヒ フ ヘ ホ マ ミ ム メ モ ヤ ユ ヨ ラ リ ル レ ロ ワ ヲ ン ャ ュ ョ ァ ィ ゥ ェ ォ ッ ゛ ゜ ー 。 「 」 、 ・ ！ ？).freeze
-    @@external = %w(ｶﾞ ｷﾞ ｸﾞ ｹﾞ ｺﾞ ｻﾞ ｼﾞ ｽﾞ ｾﾞ ｿﾞ ﾀﾞ ﾁﾞ ﾂﾞ ﾃﾞ ﾄﾞ ﾊﾞ ﾋﾞ ﾌﾞ ﾍﾞ ﾎﾞ ﾊﾟ ﾋﾟ ﾌﾟ ﾍﾟ ﾎﾟ ｳﾞ ｱ ｲ ｳ ｴ ｵ ｶ ｷ ｸ ｹ ｺ ｻ ｼ ｽ ｾ ｿ ﾀ ﾁ ﾂ ﾃ ﾄ ﾅ ﾆ ﾇ ﾈ ﾉ ﾊ ﾋ ﾌ ﾍ ﾎ ﾏ ﾐ ﾑ ﾒ ﾓ ﾔ ﾕ ﾖ ﾗ ﾘ ﾙ ﾚ ﾛ ﾜ ｦ ﾝ ｬ ｭ ｮ ｧ ｨ ｩ ｪ ｫ ｯ ﾞ ﾟ ｰ ｡ ｢ ｣ ､ ･ ! ?).freeze
+    @@zen_han = {"ガ" => "ｶﾞ", "ギ" => "ｷﾞ", "グ" => "ｸﾞ", "ゲ" => "ｹﾞ", "ゴ" => "ｺﾞ", "ザ" => "ｻﾞ", "ジ" => "ｼﾞ", "ズ" => "ｽﾞ", "ゼ" => "ｾﾞ", "ゾ" => "ｿﾞ", "ダ" => "ﾀﾞ", "ヂ" => "ﾁﾞ", "ヅ" => "ﾂﾞ", "デ" => "ﾃﾞ", "ド" => "ﾄﾞ", "バ" => "ﾊﾞ", "ビ" => "ﾋﾞ", "ブ" => "ﾌﾞ", "ベ" => "ﾍﾞ", "ボ" => "ﾎﾞ", "パ" => "ﾊﾟ", "ピ" => "ﾋﾟ", "プ" => "ﾌﾟ", "ペ" => "ﾍﾟ", "ポ" => "ﾎﾟ", "ヴ" => "ｳﾞ", "ア" => "ｱ", "イ" => "ｲ", "ウ" => "ｳ", "エ" => "ｴ", "オ" => "ｵ", "カ" => "ｶ", "キ" => "ｷ", "ク" => "ｸ", "ケ" => "ｹ", "コ" => "ｺ", "サ" => "ｻ", "シ" => "ｼ", "ス" => "ｽ", "セ" => "ｾ", "ソ" => "ｿ", "タ" => "ﾀ", "チ" => "ﾁ", "ツ" => "ﾂ", "テ" => "ﾃ", "ト" => "ﾄ", "ナ" => "ﾅ", "ニ" => "ﾆ", "ヌ" => "ﾇ", "ネ" => "ﾈ", "ノ" => "ﾉ", "ハ" => "ﾊ", "ヒ" => "ﾋ", "フ" => "ﾌ", "ヘ" => "ﾍ", "ホ" => "ﾎ", "マ" => "ﾏ", "ミ" => "ﾐ", "ム" => "ﾑ", "メ" => "ﾒ", "モ" => "ﾓ", "ヤ" => "ﾔ", "ユ" => "ﾕ", "ヨ" => "ﾖ", "ラ" => "ﾗ", "リ" => "ﾘ", "ル" => "ﾙ", "レ" => "ﾚ", "ロ" => "ﾛ", "ワ" => "ﾜ", "ヲ" => "ｦ", "ン" => "ﾝ", "ャ" => "ｬ", "ュ" => "ｭ", "ョ" => "ｮ", "ァ" => "ｧ", "ィ" => "ｨ", "ゥ" => "ｩ", "ェ" => "ｪ", "ォ" => "ｫ", "ッ" => "ｯ", "゛" => "ﾞ", "゜" => "ﾟ", "ー" => "ｰ", "。" => "｡", "「" => "｢", "」" => "｣", "、" => "､", "・" => "･", "！" => "!", "？" => "?"}
+    @@han_zen = @@zen_han.invert
+
     def to_internal(str, options = {})
-      filter(str, @@external, @@internal)
+      filter(str, @@han_zen)
     end
     def to_external(str, options = {})
       unless options[:input]
-        filter(str, @@internal, @@external)
+        filter(str, @@zen_han)
       else
         encoding = (str =~ /^\s*<[^Hh>]*html/) and str.respond_to?(:encoding)
         nokogiri_klass =
@@ -73,7 +74,7 @@ module Jpmobile
       end
     end
 
-    def filter(str, from, to)
+    def filter(str, table)
       str = str.clone
 
       # 一度UTF-8に変換
@@ -83,9 +84,7 @@ module Jpmobile
         str.force_encoding("UTF-8")
       end
 
-      from.each_with_index do |int, i|
-        str.gsub!(int, to[i])
-      end
+      str = replace_chars(str, table)
 
       # 元に戻す
       if before_encoding
@@ -95,17 +94,22 @@ module Jpmobile
       str
     end
 
+    def replace_chars(str, table)
+      @regexp_cache ||= {}
+      str.gsub(@regexp_cache[table.object_id] ||= Regexp.union(table.keys), table)
+    end
+
     # 再帰的に探す
     def convert_text_content(document)
       document.children.each do |element|
         if element.kind_of?(Nokogiri::XML::Text)
           unless element.parent.node_name == "textarea"
             # textarea 以外のテキストなら content を変換
-            element.content = filter(element.content, @@internal, @@external)
+            element.content = filter(element.content, @@zen_han)
           end
         elsif element.node_name == "input" and ["submit", "reset", "button"].include?(element["type"])
           # テキスト以外でもボタンの value は変換
-          element["value"] = filter(element["value"], @@internal, @@external)
+          element["value"] = filter(element["value"], @@han_zen)
         elsif element.children.any?
           # 子要素があれば再帰的に変換
           element = convert_text_content(element)

67499393b8ee31a7241b5940bb5990af81ec2af7
akicho8 <akicho8@gmail.com>
2013-08-10 21:38:50 +0900

    HankakuFilter refactoring

--- lib/jpmobile/filter.rb
+++ lib/jpmobile/filter.rb
@@ -2,12 +2,10 @@
 # = 半角変換フィルター
 # thanks to masuidrive <masuidrive (at) masuidrive.jp>
 
-class ActionController::Base #:nodoc:
+ActiveSupport.on_load(:action_controller) do
   def self.hankaku_filter(options={})
-    options = {:input => false}.update(options)
-
-    before_filter lambda {|controller| Jpmobile::HankakuFilter.before(controller, options)}
-    after_filter  lambda {|controller| Jpmobile::HankakuFilter.after(controller, options)}
+    before_action Jpmobile::HankakuFilter.new(options)
+    after_action  Jpmobile::HankakuFilter.new(options)
   end
 
   def self.mobile_filter(options={})
@@ -18,44 +16,74 @@ class ActionController::Base #:nodoc:
 end
 
 module Jpmobile
-  module HankakuFilter
-    module_function
+  class HankakuFilter
+    cattr_accessor(:zen_to_han_table) { {"ガ" => "ｶﾞ", "ギ" => "ｷﾞ", "グ" => "ｸﾞ", "ゲ" => "ｹﾞ", "ゴ" => "ｺﾞ", "ザ" => "ｻﾞ", "ジ" => "ｼﾞ", "ズ" => "ｽﾞ", "ゼ" => "ｾﾞ", "ゾ" => "ｿﾞ", "ダ" => "ﾀﾞ", "ヂ" => "ﾁﾞ", "ヅ" => "ﾂﾞ", "デ" => "ﾃﾞ", "ド" => "ﾄﾞ", "バ" => "ﾊﾞ", "ビ" => "ﾋﾞ", "ブ" => "ﾌﾞ", "ベ" => "ﾍﾞ", "ボ" => "ﾎﾞ", "パ" => "ﾊﾟ", "ピ" => "ﾋﾟ", "プ" => "ﾌﾟ", "ペ" => "ﾍﾟ", "ポ" => "ﾎﾟ", "ヴ" => "ｳﾞ", "ア" => "ｱ", "イ" => "ｲ", "ウ" => "ｳ", "エ" => "ｴ", "オ" => "ｵ", "カ" => "ｶ", "キ" => "ｷ", "ク" => "ｸ", "ケ" => "ｹ", "コ" => "ｺ", "サ" => "ｻ", "シ" => "ｼ", "ス" => "ｽ", "セ" => "ｾ", "ソ" => "ｿ", "タ" => "ﾀ", "チ" => "ﾁ", "ツ" => "ﾂ", "テ" => "ﾃ", "ト" => "ﾄ", "ナ" => "ﾅ", "ニ" => "ﾆ", "ヌ" => "ﾇ", "ネ" => "ﾈ", "ノ" => "ﾉ", "ハ" => "ﾊ", "ヒ" => "ﾋ", "フ" => "ﾌ", "ヘ" => "ﾍ", "ホ" => "ﾎ", "マ" => "ﾏ", "ミ" => "ﾐ", "ム" => "ﾑ", "メ" => "ﾒ", "モ" => "ﾓ", "ヤ" => "ﾔ", "ユ" => "ﾕ", "ヨ" => "ﾖ", "ラ" => "ﾗ", "リ" => "ﾘ", "ル" => "ﾙ", "レ" => "ﾚ", "ロ" => "ﾛ", "ワ" => "ﾜ", "ヲ" => "ｦ", "ン" => "ﾝ", "ャ" => "ｬ", "ュ" => "ｭ", "ョ" => "ｮ", "ァ" => "ｧ", "ィ" => "ｨ", "ゥ" => "ｩ", "ェ" => "ｪ", "ォ" => "ｫ", "ッ" => "ｯ", "゛" => "ﾞ", "゜" => "ﾟ", "ー" => "ｰ", "。" => "｡", "「" => "｢", "」" => "｣", "、" => "､", "・" => "･", "！" => "!", "？" => "?"} }
+
+    class << self
+      def hankaku_format(str)
+        replace_chars(str, zen_to_han_table)
+      end
+
+      def zenkaku_format(str)
+        replace_chars(str, han_to_zen_table)
+      end
+
+      private
+
+      def replace_chars(str, table)
+        @regexp_cache ||= {}
+        str.gsub(@regexp_cache[table.object_id] ||= Regexp.union(table.keys), table)
+      end
+
+      def han_to_zen_table
+        @han_to_zen_table ||= zen_to_han_table.invert
+      end
+    end
+
+    def initialize(options = {})
+      @options = {
+        :input => false,
+      }.merge(options)
+
+      @controller = nil
+    end
+
+    def before(controller)
+      @controller = controller
+      if apply_incoming?
+        Util.deep_apply(@controller.params) do |value|
+          value = to_internal(value)
+        end
+      end
+    end
+
+    # 内部コードから外部コードに変換
+    def after(controller)
+      @controller = controller
+      if apply_outgoing? and @controller.response.body.is_a?(String)
+        @controller.response.body = to_external(@controller.response.body)
+      end
+    end
+
+    private
 
     # 入出力フィルタの適用条件
-    def apply_incoming?(controller)
-      controller.request.mobile?
-    end
-    def apply_outgoing?(controller)
-      controller.request.mobile? and
-        [nil, "text/html", "application/xhtml+xml"].include?(controller.response.content_type)
+    def apply_incoming?
+      @controller.request.mobile?
     end
 
-    def before(controller, options = {})
-      if apply_incoming?(controller)
-        Util.deep_apply(controller.params) do |value|
-          value = to_internal(value, options)
-        end
-      end
-    end
-    # 内部コードから外部コードに変換
-    def after(controller, options = {})
-      if apply_outgoing?(controller) and controller.response.body.is_a?(String)
-        if controller.request.mobile?
-          options.merge!(:charset => controller.request.mobile.default_charset)
-        end
-        controller.response.body = to_external(controller.response.body, options)
-      end
+    def apply_outgoing?
+      @controller.request.mobile? and
+        [nil, "text/html", "application/xhtml+xml"].include?(@controller.response.content_type)
     end
 
-    @@zen_han = {"ガ" => "ｶﾞ", "ギ" => "ｷﾞ", "グ" => "ｸﾞ", "ゲ" => "ｹﾞ", "ゴ" => "ｺﾞ", "ザ" => "ｻﾞ", "ジ" => "ｼﾞ", "ズ" => "ｽﾞ", "ゼ" => "ｾﾞ", "ゾ" => "ｿﾞ", "ダ" => "ﾀﾞ", "ヂ" => "ﾁﾞ", "ヅ" => "ﾂﾞ", "デ" => "ﾃﾞ", "ド" => "ﾄﾞ", "バ" => "ﾊﾞ", "ビ" => "ﾋﾞ", "ブ" => "ﾌﾞ", "ベ" => "ﾍﾞ", "ボ" => "ﾎﾞ", "パ" => "ﾊﾟ", "ピ" => "ﾋﾟ", "プ" => "ﾌﾟ", "ペ" => "ﾍﾟ", "ポ" => "ﾎﾟ", "ヴ" => "ｳﾞ", "ア" => "ｱ", "イ" => "ｲ", "ウ" => "ｳ", "エ" => "ｴ", "オ" => "ｵ", "カ" => "ｶ", "キ" => "ｷ", "ク" => "ｸ", "ケ" => "ｹ", "コ" => "ｺ", "サ" => "ｻ", "シ" => "ｼ", "ス" => "ｽ", "セ" => "ｾ", "ソ" => "ｿ", "タ" => "ﾀ", "チ" => "ﾁ", "ツ" => "ﾂ", "テ" => "ﾃ", "ト" => "ﾄ", "ナ" => "ﾅ", "ニ" => "ﾆ", "ヌ" => "ﾇ", "ネ" => "ﾈ", "ノ" => "ﾉ", "ハ" => "ﾊ", "ヒ" => "ﾋ", "フ" => "ﾌ", "ヘ" => "ﾍ", "ホ" => "ﾎ", "マ" => "ﾏ", "ミ" => "ﾐ", "ム" => "ﾑ", "メ" => "ﾒ", "モ" => "ﾓ", "ヤ" => "ﾔ", "ユ" => "ﾕ", "ヨ" => "ﾖ", "ラ" => "ﾗ", "リ" => "ﾘ", "ル" => "ﾙ", "レ" => "ﾚ", "ロ" => "ﾛ", "ワ" => "ﾜ", "ヲ" => "ｦ", "ン" => "ﾝ", "ャ" => "ｬ", "ュ" => "ｭ", "ョ" => "ｮ", "ァ" => "ｧ", "ィ" => "ｨ", "ゥ" => "ｩ", "ェ" => "ｪ", "ォ" => "ｫ", "ッ" => "ｯ", "゛" => "ﾞ", "゜" => "ﾟ", "ー" => "ｰ", "。" => "｡", "「" => "｢", "」" => "｣", "、" => "､", "・" => "･", "！" => "!", "？" => "?"}
-    @@han_zen = @@zen_han.invert
-
-    def to_internal(str, options = {})
-      filter(str, @@han_zen)
+    def to_internal(str)
+      filter(:zenkaku, str)
     end
-    def to_external(str, options = {})
-      unless options[:input]
-        filter(str, @@zen_han)
+
+    def to_external(str)
+      unless @options[:input]
+        filter(:hankaku, str)
       else
         encoding = (str =~ /^\s*<[^Hh>]*html/) and str.respond_to?(:encoding)
         nokogiri_klass =
@@ -69,12 +97,12 @@ module Jpmobile
         doc = convert_text_content(doc)
 
         html = doc.to_html.gsub("\xc2\xa0","&nbsp;")
-        html = html.gsub(/charset=[a-z0-9\-]+/i, "charset=#{options[:charset]}") if options[:charset]
+        html = html.gsub(/charset=[a-z0-9\-]+/i, "charset=#{default_charset}") if default_charset
         html
       end
     end
 
-    def filter(str, table)
+    def filter(method, str)
       str = str.clone
 
       # 一度UTF-8に変換
@@ -84,7 +112,7 @@ module Jpmobile
         str.force_encoding("UTF-8")
       end
 
-      str = replace_chars(str, table)
+      str = self.class.send("#{method}_format", str)
 
       # 元に戻す
       if before_encoding
@@ -94,22 +122,17 @@ module Jpmobile
       str
     end
 
-    def replace_chars(str, table)
-      @regexp_cache ||= {}
-      str.gsub(@regexp_cache[table.object_id] ||= Regexp.union(table.keys), table)
-    end
-
     # 再帰的に探す
     def convert_text_content(document)
       document.children.each do |element|
         if element.kind_of?(Nokogiri::XML::Text)
           unless element.parent.node_name == "textarea"
             # textarea 以外のテキストなら content を変換
-            element.content = filter(element.content, @@zen_han)
+            element.content = filter(:hankaku, element.content)
           end
         elsif element.node_name == "input" and ["submit", "reset", "button"].include?(element["type"])
           # テキスト以外でもボタンの value は変換
-          element["value"] = filter(element["value"], @@han_zen)
+          element["value"] = filter(:zenkaku, element["value"])
         elsif element.children.any?
           # 子要素があれば再帰的に変換
           element = convert_text_content(element)
@@ -117,6 +140,12 @@ module Jpmobile
       end
 
       document
+    end
+
+    def default_charset
+      if @controller.request.mobile?
+        @controller.request.mobile.default_charset
+      end
     end
   end
 end

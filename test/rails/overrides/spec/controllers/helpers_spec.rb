df33655c860f052a314495e2da1680daef08f55d
Shinichiro OGAWA <shinichiro-ogawa@cookpad.com>
2013-12-19 16:49:16 +0900

    Fix helpers_spec to move to controller

--- test/rails/overrides/spec/controllers/helpers_spec.rb
+++ test/rails/overrides/spec/controllers/helpers_spec.rb
@@ -2,7 +2,9 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '/../spec_helper'))
 require 'nokogiri'
 
-describe Jpmobile::Helpers do
+describe LinksController do
+  render_views
+
   # 文字列 +str+ 中に含まれるリンクについて、
   # リンクテキスト、属性のHash、URLのqueryをのぞいた部分、ueryをHashにしたもの
   # の3要素からなる配列の配列で返す。
@@ -17,37 +19,41 @@ describe Jpmobile::Helpers do
   end
 
   it "get_position_link_to_がエラー無く終わるか" do
+    request.user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"
     lambda {
-      get "/links/show_all", {}, {"HTTP_USER_AGENT" => "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"}
+      get :show_all
     }.should_not raise_error
   end
 
   context "docomo で" do
     it "get_position_link_to が正常に表示されること" do
-      get "/links/link", {}, { "HTTP_USER_AGENT" => "DoCoMo/2.0 SH903i(c100;TB;W24H16)"}
-      links = get_href_and_texts(body)
+      request.user_agent = "DoCoMo/2.0 SH903i(c100;TB;W24H16)"
+      get :link
+      links = get_href_and_texts(response.body)
 
       links.size.should == 1
       text, attrs, path, params = links.first
       text.should == "STRING"
-      path.should == "http://www.example.com/links/link"
-      body.should =~ /lcs>/
+      path.should == "http://test.host/links/link"
+      response.body.should =~ /lcs>/
     end
 
     it "docomo_utn_link_to が正しく出力されること" do
-      get "/links/docomo_utn", {}, {"HTTP_USER_AGENT" => "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"}
-      links = get_href_and_texts(body)
+      request.user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"
+      get :docomo_utn
+      links = get_href_and_texts(response.body)
 
       links.size.should == 1
       text, attrs, path, params = links.first
       text.should == "STRING"
-      path.should == "http://www.example.com/links/docomo_utn"
-      body.should =~ /utn>/
+      path.should == "http://test.host/links/docomo_utn"
+      response.body.should =~ /utn>/
     end
 
     it "オープンiエリア取得用のリンクが正しく出力されること" do
-      get "/links/docomo_openiarea", {}, {"HTTP_USER_AGENT" => "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"}
-      links = get_href_and_texts(body)
+      request.user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"
+      get :docomo_openiarea
+      links = get_href_and_texts(response.body)
 
       links.size.should == 1
       text, attrs, path, params = links.first
@@ -56,42 +62,45 @@ describe Jpmobile::Helpers do
       params["ecode"].should   == "OPENAREACODE"
       params["msn"].should     == "OPENAREAKEY"
       params["posinfo"].should == "1"
-      params["nl"].should      == "http://www.example.com/links/docomo_openiarea"
+      params["nl"].should      == "http://test.host/links/docomo_openiarea"
     end
 
     it "GPS取得用のリンクが正しく出力されること" do
-      get "/links/docomo_foma_gps", {}, {"HTTP_USER_AGENT" => "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"}
-      links = get_href_and_texts(body)
+      request.user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"
+      get :docomo_foma_gps
+      links = get_href_and_texts(response.body)
 
       links.size.should == 1
       text, attrs, path, params = links.first
       text.should == "STRING"
-      path.should == "http://www.example.com/links/docomo_foma_gps"
-      body.should =~ /lcs>/
+      path.should == "http://test.host/links/docomo_foma_gps"
+      response.body.should =~ /lcs>/
     end
   end
 
   context "au で" do
     # get_position_link_to(自動判別), au, location only
     def test_get_position_link_to_au_location_only
-      get "/links/link", {}, {"HTTP_USER_AGENT" => "KDDI-SN26 UP.Browser/6.2.0.6.2 (GUI) MMP/2.0"}
-      links = get_href_and_texts(body)
+      request.user_agent = "KDDI-SN26 UP.Browser/6.2.0.6.2 (GUI) MMP/2.0"
+      get :link
+      links = get_href_and_texts(response.body)
       assert_equal(1, links.size)
       text, attrs, path, params = links.first
       assert_equal("STRING", text)
       assert_equal("device:location", path)
-      assert_equal("http://www.example.com/links/link", params["url"])
+      assert_equal("http://test.host/links/link", params["url"])
     end
 
     # get_position_link_to(自動判別), au, gps
     def test_get_position_link_to_au_gps
-      get "/links/link", {}, {"HTTP_USER_AGENT" => "KDDI-CA32 UP.Browser/6.2.0.7.3.129 (GUI) MMP/2.0"}
-      links = get_href_and_texts(body)
+      request.user_agent = "KDDI-CA32 UP.Browser/6.2.0.7.3.129 (GUI) MMP/2.0"
+      get :link
+      links = get_href_and_texts(response.body)
       assert_equal(1, links.size)
       text, attrs, path, params = links.first
       assert_equal("STRING", text)
       assert_equal("device:gpsone", path)
-      assert_equal("http://www.example.com/links/link", params["url"])
+      assert_equal("http://test.host/links/link", params["url"])
       assert_equal("0", params["number"])
       assert_equal("0", params["acry"])
       assert_equal("1", params["ver"])
@@ -101,24 +110,26 @@ describe Jpmobile::Helpers do
 
     # au簡易位置情報取得用のリンクが正しく出力されるか。
     def test_au_location_link_to
-      get "/links/au_location", {}, {"HTTP_USER_AGENT" => "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"}
-      links = get_href_and_texts(body)
+      request.user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"
+      get :au_location
+      links = get_href_and_texts(response.body)
       assert_equal(1, links.size)
       text, attrs, path, params = links.first
       assert_equal("STRING", text)
       assert_equal("device:location", path)
-      assert_equal("http://www.example.com/links/au_location", params["url"])
+      assert_equal("http://test.host/links/au_location", params["url"])
     end
 
     # au GPS位置情報取得用のリンクが正しく出力されるか。
     def test_au_gps_link_to
-      get "/links/au_gps", {}, {"HTTP_USER_AGENT" => "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"}
-      links = get_href_and_texts(body)
+      request.user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"
+      get :au_gps
+      links = get_href_and_texts(response.body)
       assert_equal(1, links.size)
       text, attrs, path, params = links.first
       assert_equal("STRING", text)
       assert_equal("device:gpsone", path)
-      assert_equal("http://www.example.com/links/au_gps", params["url"])
+      assert_equal("http://test.host/links/au_gps", params["url"])
       assert_equal("0", params["number"])
       assert_equal("0", params["acry"])
       assert_equal("1", params["ver"])
@@ -130,8 +141,9 @@ describe Jpmobile::Helpers do
   context "softbank で" do
     # get_position_link_to(自動判別), Vodafone
     def test_get_position_link_to_vodafone
-      get "/links/link", {}, {"HTTP_USER_AGENT" => "Vodafone/1.0/V903T/TJ001 Browser/VF-Browser/1.0 Profile/MIDP-2.0 Configuration/CLDC-1.1 Ext-J-Profile/JSCL-1.2.2 Ext-V-Profile/VSCL-2.0.0"}
-      links = get_href_and_texts(body)
+      request.user_agent = "Vodafone/1.0/V903T/TJ001 Browser/VF-Browser/1.0 Profile/MIDP-2.0 Configuration/CLDC-1.1 Ext-J-Profile/JSCL-1.2.2 Ext-V-Profile/VSCL-2.0.0"
+      get :link
+      links = get_href_and_texts(response.body)
       assert_equal(1, links.size)
       text, attrs, path, params = links.first
       assert_equal("STRING", text)
@@ -140,8 +152,9 @@ describe Jpmobile::Helpers do
 
     # get_position_link_to(自動判別), Softbank
     def test_get_position_link_to_softbank
-      get "/links/link", {}, {"HTTP_USER_AGENT" => "SoftBank/1.0/910T/TJ001/SN000000000000000 Browser/NetFront/3.3 Profile/MIDP-2.0 Configuration/CLDC-1.1"}
-      links = get_href_and_texts(body)
+      request.user_agent = "SoftBank/1.0/910T/TJ001/SN000000000000000 Browser/NetFront/3.3 Profile/MIDP-2.0 Configuration/CLDC-1.1"
+      get :link
+      links = get_href_and_texts(response.body)
       assert_equal(1, links.size)
       text, attrs, path, params = links.first
       assert_equal("STRING", text)
@@ -150,8 +163,9 @@ describe Jpmobile::Helpers do
 
     # Softbank 3G 位置情報取得用のリンクが正しく出力されるか。
     def test_softbank_location_link_to
-      get "/links/softbank_location", {}, {"HTTP_USER_AGENT" => "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"}
-      links = get_href_and_texts(body)
+      request.user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"
+      get :softbank_location
+      links = get_href_and_texts(response.body)
       assert_equal(1, links.size)
       text, attrs, path, params = links.first
       assert_equal("STRING", text)
@@ -162,25 +176,27 @@ describe Jpmobile::Helpers do
   context "willcom で" do
     # get_position_link_to(自動判別), Willcom
     def test_get_position_link_to_willcom
-      get "/links/link", {}, {"HTTP_USER_AGENT" => "Mozilla/3.0(WILLCOM;KYOCERA/WX310K/2;1.2.2.16.000000/0.1/C100) Opera 7.0"}
-      links = get_href_and_texts(body)
+      request.user_agent = "Mozilla/3.0(WILLCOM;KYOCERA/WX310K/2;1.2.2.16.000000/0.1/C100) Opera 7.0"
+      get :link
+      links = get_href_and_texts(response.body)
       assert_equal(1, links.size)
       text, attrs, path, params = links.first
       assert_equal("STRING", text)
       assert_equal("http://location.request/dummy.cgi", path)
-      assert_equal("http://www.example.com/links/link", params["my"])
+      assert_equal("http://test.host/links/link", params["my"])
       assert_equal("$location", params["pos"])
     end
 
     # Willcom 位置情報取得用のリンクが正しく出力されるか。
     def test_willcom_location_link_to
-      get "/links/willcom_location", {}, {"HTTP_USER_AGENT" => "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"}
-      links = get_href_and_texts(body)
+      request.user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"
+      get :willcom_location
+      links = get_href_and_texts(response.body)
       assert_equal(1, links.size)
       text, attrs, path, params = links.first
       assert_equal("STRING", text)
       assert_equal("http://location.request/dummy.cgi", path)
-      assert_equal("http://www.example.com/links/willcom_location", params["my"])
+      assert_equal("http://test.host/links/willcom_location", params["my"])
       assert_equal("$location", params["pos"])
     end
   end

006edc546278c4239d861b2f216fae35f9fe4cb1
Akira Matsuda <ronnie@dio.jp>
2014-06-03 18:41:41 +0900

    Apply GetText filter only when GetText is defined

--- lib/jpmobile/hook_action_controller.rb
+++ lib/jpmobile/hook_action_controller.rb
@@ -4,13 +4,16 @@ require 'jpmobile/lookup_context'
 module ActionController
   class Base
     include Jpmobile::Helpers
-    before_action :gettext_force_ja_for_mobile
-    # gettextが組み込まれている場合、携帯電話からのアクセスをjaロケールに強制する。
-    def gettext_force_ja_for_mobile
-      if Object.const_defined?(:GetText) and request.mobile?
-        begin
-          ::GetText.locale = 'ja'
-        rescue NameError
+
+    if Object.const_defined?(:GetText)
+      before_action :gettext_force_ja_for_mobile
+      # gettextが組み込まれている場合、携帯電話からのアクセスをjaロケールに強制する。
+      def gettext_force_ja_for_mobile
+        if request.mobile?
+          begin
+            ::GetText.locale = 'ja'
+          rescue NameError
+          end
         end
       end
     end

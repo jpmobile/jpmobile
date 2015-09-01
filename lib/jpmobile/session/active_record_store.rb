4fcb7a661af6c07cd0f0ee619513960fb69bca29
Shin-ichiro OGAWA <shinichiro-ogawa@cookpad.com>
2015-09-01 18:24:07 +0900

    Use ActionDispatch::Session::ActiveRecordStore

--- lib/jpmobile/session/active_record_store.rb
+++ lib/jpmobile/session/active_record_store.rb
@@ -1,11 +1,13 @@
-module ActiveRecord
-  class SessionStore
-    def destroy_session_with_jpmobile(env, session_id, options)
-      destroy_session_without_jpmobile(env, session_id, options)
+module ActionDispatch
+  module Session
+    class ActiveRecordStore
+      def destroy_session_with_jpmobile(env, session_id, options)
+        destroy_session_without_jpmobile(env, session_id, options)
 
-      session_id || Jpmobile::SessionID.generate_sid
+        session_id || Jpmobile::SessionID.generate_sid
+      end
+
+      alias_method_chain :destroy_session, :jpmobile
     end
-
-    alias_method_chain :destroy_session, :jpmobile
   end
 end

08a528ffa31ad9fe9175e532677cf1e6a7acc071
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2015-12-31 14:19:32 +0900

    Add :host option implicitly

--- test/rails/overrides/spec/helpers/helpers_spec.rb
+++ test/rails/overrides/spec/helpers/helpers_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-describe Jpmobile::Helpers, :type => :helper do
+describe Jpmobile::Helpers, type: :helper do
   include Jpmobile::Helpers
+  include Rails.application.routes.url_helpers
+
   it "docomo_guid_link_to が guid=ON を付けたリンクを生成すること" do
-    expect(docomo_guid_link_to("STRING", :controller => "filter", :action => "rawdata")).to eq(%{<a href="/filter/rawdata?guid=ON">STRING</a>})
+    expect(docomo_guid_link_to("STRING", host: 'jpmobile.info', controller: "filter", action: "rawdata")).to eq(%{<a href="http://jpmobile.info/filter/rawdata?guid=ON">STRING</a>})
   end
 
   it "softbank_location_link_to がリンク先にパラメータを含んでいても正常に動作すること" do
     # http://d.hatena.ne.jp/mizincogrammer/20090123/1232702067
-    expect(softbank_location_link_to("STRING", :controller => "filter", :action => "rawdata", :p => "param")).to eq(%{<a href="location:auto?url=http://test.host/filter/rawdata&amp;p=param">STRING</a>})
+    expect(softbank_location_link_to("STRING", host: 'jpmobile.info', controller: "filter", action: "rawdata", p: "param")).to eq(%{<a href="location:auto?url=http://jpmobile.info/filter/rawdata&amp;p=param">STRING</a>})
   end
 end

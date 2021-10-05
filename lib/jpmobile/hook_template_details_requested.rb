d44d93bbc71df9bff85ebc1eb96f75a98663afcc
Shin-ichiro OGAWA <rust.stnard@gmail.com>
2021-10-05 19:57:26 +0900

    Implement TemplateDetails and some classes
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard@gmail.com>

---
+++ lib/jpmobile/hook_template_details_requested.rb
@@ -0,0 +1,12 @@
+module Jpmobile
+  module HookTemplateDetailsRequested
+    attr_reader :mobile, :mobile_idx
+
+    def initialize(locale:, handlers:, formats:, variants:, mobile:)
+      super(locale: locale, handlers: handlers, formats: formats, variants: variants)
+
+      @mobile = mobile.map(&:to_sym)
+      @mobile_idx  = build_idx_hash(@mobile)
+    end
+  end
+end
---
+++ lib/jpmobile/template_details.rb
@@ -0,0 +1,38 @@
+module Jpmobile
+  class TemplateDetails < ActionView::TemplateDetails
+    def initialize(locale, handler, format, variant, mobile)
+      @mobile = mobile
+
+      super(locale, handler, format, variant)
+    end
+
+    def matches?(requested)
+      requested.formats_idx[@format] &&
+        requested.locale_idx[@locale] &&
+        requested.variants_idx[@variant] &&
+        requested.handlers_idx[@handler] &&
+        requested.mobile_idx[@mobile]
+    end
+
+    def sort_key_for(requested)
+      [
+        requested.formats_idx[@format],
+        requested.locale_idx[@locale],
+        requested.variants_idx[@variant],
+        requested.handlers_idx[@handler],
+        requested.mobile_idx[@mobile]
+      ]
+    end
+
+    class Requested < ActionView::TemplateDetails::Requested
+      attr_reader :mobile, :mobile_idx
+
+      def initialize(locale:, handlers:, formats:, variants:, mobile:)
+        super(locale: locale, handlers: handlers, formats: formats, variants: variants)
+
+        @mobile = mobile.map(&:to_sym)
+        @mobile_idx  = build_idx_hash(mobile)
+      end
+    end
+  end
+end

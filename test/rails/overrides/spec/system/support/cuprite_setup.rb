0671bfd7060621c291f4fb9ecfb5821d4c118829
Shin-ichiro OGAWA <rust.stnard@gmail.com>
2023-03-21 23:15:39 +0900

    Fix for Style/RedundantDoubleSplatHashBraces
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard@gmail.com>

--- test/rails/overrides/spec/system/support/cuprite_setup.rb
+++ test/rails/overrides/spec/system/support/cuprite_setup.rb
@@ -3,11 +3,9 @@ require 'capybara/cuprite'
 Capybara.register_driver(:cuprite) do |app|
   Capybara::Cuprite::Driver.new(
     app,
-    **{
-      window_size: [1200, 800],
-      browser_options: {},
-      inspector: true,
-    },
+    window_size: [1200, 800],
+    browser_options: {},
+    inspector: true,
   )
 end
 

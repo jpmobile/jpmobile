e921078da2e54c726900efae09302b1767026cda
Shin-ichiro OGAWA <rust.stnard@gmail.com>
2024-06-13 11:00:51 +0900

    Pluralize
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard@gmail.com>

--- test/rails/overrides/spec/rails_helper.rb
+++ test/rails/overrides/spec/rails_helper.rb
@@ -28,7 +28,9 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{Rails.root}/spec/fixtures"
+  config.fixture_paths = [
+    "#{Rails.root}/spec/fixtures",
+  ]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
